0d8913dca387de948e14877ee5b0d81972ad0c6a
Duke0404 <01165826@pw.edu.pl>
2023-03-29 22:39:53 +0200

    Added JWT Implementation without libraries

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-import { createServer } from "http"
+// import express from "express"
+// import dotenv from "dotenv"
+// import jwt from "jsonwebtoken"
 
-const hostname = "0.0.0.0"
-const port = 3000
+// port = process.env.PORT || 3000
 
-const server = createServer((req, res) => {
-	res.statusCode = 200
-	res.setHeader("Content-Type", "text/plain")
-	res.end("Okay\n")
-})
+// dotenv.config()
+// const app = express()
+// app.use(express.json())
 
-// GET request on http://localhost:3000/
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`)
-})
+// const usersDB = [
+// 	{ username: "admin", password: "admin" },
+// 	{ username: "user", password: "user" }
+// ]
+
+// // Login route
+// app.post("/login", (req, res) => {
+// 	const { username, password } = req.body
+
+// 	if (!username || !password) return res.status(400).json({ error: "Missing credentials" })
+
+// 	if (!usersDB.find(user => user.username === username && user.password === password))
+// 		return res.status(401).json({ error: "Invalid credentials" })
+
+// 	let token
+
+// 	try {
+// 		token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "12h" })
+// 	} catch (err) {
+// 		return res.status(500).json({ error: "Internal server error" })
+// 	}
+
+// 	res.json({ token })
+// })
+
+// // Signup route
+// app.post("/signup", (req, res) => {
+// 	const { username, password } = req.body
+
+// 	if (!username || !password) return res.status(400).json({ error: "Missing credentials" })
+
+// 	if (usersDB.find(user => user.username === username))
+// 		return res.status(409).json({ error: "Use a different username" })
+
+// 	usersDB.push({ username, password })
+
+// 	let token
+
+// 	try {
+// 		token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "12h" })
+// 	} catch (err) {
+// 		return res.status(500).json({ error: "Internal server error" })
+// 	}
+
+// 	return res.json({ message: "User created successfully" })
+// })
+
+// app.listen(port, () => console.log("Server running on http://localhost:" + port + " ⚡"))
+
---
+++ utils/jsonWebToken.js
@@ -0,0 +1,43 @@
+import { createHmac } from "crypto"
+
+const toBase64 = object => Buffer.from(JSON.stringify(object)).toString("base64")
+const fromBase64 = b64str => JSON.parse(Buffer.from(b64str, "base64").toString())
+
+function replaceChars(b64str) {
+	const replacements = {
+		"=": "",
+		"+": "-",
+		"/": "_"
+	}
+
+	return b64str.replace(/[=+/]/g, match => replacements[match])
+}
+
+const generateSignature = (header, payload, secret) =>
+	replaceChars(
+		createHmac("sha256", secret)
+			.update(header + "." + payload)
+			.digest("base64")
+	)
+
+export function generateJWT(payload, secret, expiresIn) {
+	const headerJWT = replaceChars(toBase64({ alg: "HS256", typ: "JWT" }))
+	const payloadJWT = replaceChars(
+		toBase64({ ...payload, exp: Math.floor(Date.now() / 1000) + 3600 * expiresIn })
+	)
+	const signatureJWT = generateSignature(headerJWT, payloadJWT, secret)
+
+	return headerJWT + "." + payloadJWT + "." + signatureJWT
+}
+
+export function verifyJWT(token, secret) {
+	const [headerJWT, payloadJWT, signatureJWT] = token.split(".")
+
+	if (
+		signatureJWT !== generateSignature(headerJWT, payloadJWT, secret) ||
+		payload.exp < Math.floor(Date.now() / 1000)
+	)
+		return null
+
+	return fromBase64(payloadJWT)
+}
